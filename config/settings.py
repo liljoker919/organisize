64fbc86d4a885c898bd52e3d32fa07440a26a29d
liljoker919 <chrs.nickerson@gmail.com>
2025-04-08 20:12:18 -0400

    Update settings to allow local development and implement home view with HTML template

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g4)5^0ptrfuf3q$$-k&7-x-ha5u7#1a0q+zb$nbyly5+)(b89&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["3.128.45.219", "organisize.com"]
+ALLOWED_HOSTS = ["3.128.45.219", "organisize.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
