bbf9ec5a42bccc549f181eb14d32ce570367c99d
liljoker919 <chrs.nickerson@gmail.com>
2025-04-11 21:02:47 -0400

    Refactor forms and modals for improved validation and user experience

--- planner/forms.py
+++ planner/forms.py
@@ -25,44 +25,45 @@ class VacationPlanForm(forms.ModelForm):
 class FlightForm(forms.ModelForm):
     class Meta:
         model = Flight
-        fields = [
-            "airline",
-            "confirmation",
-            "departure_airport",
-            "arrival_airport",
-            "departure_time",
-            "arrival_time",
-        ]
+        exclude = ["vacation"]
         widgets = {
-            "departure_time": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "arrival_time": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "airline": forms.TextInput(attrs={"class": "form-control"}),
+            "confirmation": forms.TextInput(attrs={"class": "form-control"}),
+            "departure_airport": forms.TextInput(attrs={"class": "form-control"}),
+            "arrival_airport": forms.TextInput(attrs={"class": "form-control"}),
+            "departure_time": forms.DateTimeInput(
+                attrs={"class": "form-control", "type": "datetime-local"}
+            ),
+            "arrival_time": forms.DateTimeInput(
+                attrs={"class": "form-control", "type": "datetime-local"}
+            ),
         }
 
 
 class LodgingForm(forms.ModelForm):
     class Meta:
         model = Lodging
-        fields = [
-            "confirmation",
-            "name",
-            "check_in",
-            "check_out",
-        ]
+        exclude = ["vacation"]
         widgets = {
-            "check_in": forms.DateInput(attrs={"type": "date"}),
-            "check_out": forms.DateInput(attrs={"type": "date"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "confirmation": forms.TextInput(attrs={"class": "form-control"}),
+            "check_in": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
+            "check_out": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
         }
 
 
 class ActivityForm(forms.ModelForm):
     class Meta:
         model = Activity
-        fields = [
-            "name",
-            "date",
-            "start_time",
-        ]
+        exclude = ["vacation"]
         widgets = {
-            "date": forms.DateInput(attrs={"type": "date"}),
-            "start_time": forms.TimeInput(attrs={"type": "time"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "date": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+            "start_time": forms.TimeInput(
+                attrs={"class": "form-control", "type": "time"}
+            ),
         }
--- planner/views.py
+++ planner/views.py
@@ -3,7 +3,10 @@ from django.forms import inlineformset_factory
 from .models import VacationPlan, Flight, Lodging, Activity
 from .forms import VacationPlanForm, FlightForm, LodgingForm, ActivityForm
 from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 from .models import Flight, Lodging, Activity
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 
 def home(request):
@@ -29,15 +32,113 @@ def create_vacation(request):
 def vacation_detail(request, pk):
     vacation = get_object_or_404(VacationPlan, pk=pk)
 
-    # Query related objects
     flights = Flight.objects.filter(vacation=vacation)
     lodgings = Lodging.objects.filter(vacation=vacation)
     activities = Activity.objects.filter(vacation=vacation)
 
-    # Fresh forms for modals
     flight_form = FlightForm()
     lodging_form = LodgingForm()
     activity_form = ActivityForm()
+
+    context = {
+        "vacation": vacation,
+        "flight_form": flight_form,
+        "lodging_form": lodging_form,
+        "activity_form": activity_form,
+        "flights": flights,
+        "lodgings": lodgings,
+        "activities": activities,
+    }
+
+    return render(request, "planner/vacation_detail.html", context)
+
+
+@require_http_methods(["POST"])
+def add_flight(request, pk):
+    vacation = get_object_or_404(VacationPlan, pk=pk)
+    form = FlightForm(request.POST)
+
+    if form.is_valid():
+        flight = form.save(commit=False)
+        flight.vacation = vacation
+        flight.save()
+        return redirect("vacation_detail", pk=pk)
+
+    # If invalid, re-render the whole page with the modal visible and errors
+    lodging_form = LodgingForm()
+    activity_form = ActivityForm()
+
+    flights = Flight.objects.filter(vacation=vacation)
+    lodgings = Lodging.objects.filter(vacation=vacation)
+    activities = Activity.objects.filter(vacation=vacation)
+
+    return render(
+        request,
+        "planner/vacation_detail.html",
+        {
+            "vacation": vacation,
+            "flight_form": form,
+            "lodging_form": lodging_form,
+            "activity_form": activity_form,
+            "flights": flights,
+            "lodgings": lodgings,
+            "activities": activities,
+            "show_flight_modal": True,
+        },
+    )
+
+
+@require_http_methods(["POST"])
+def add_lodging(request, pk):
+    vacation = get_object_or_404(VacationPlan, pk=pk)
+    form = LodgingForm(request.POST)
+
+    if form.is_valid():
+        lodging = form.save(commit=False)
+        lodging.vacation = vacation
+        lodging.save()
+        return redirect("vacation_detail", pk=pk)
+
+    flight_form = FlightForm()
+    activity_form = ActivityForm()
+
+    flights = Flight.objects.filter(vacation=vacation)
+    lodgings = Lodging.objects.filter(vacation=vacation)
+    activities = Activity.objects.filter(vacation=vacation)
+
+    return render(
+        request,
+        "planner/vacation_detail.html",
+        {
+            "vacation": vacation,
+            "flight_form": flight_form,
+            "lodging_form": form,
+            "activity_form": activity_form,
+            "flights": flights,
+            "lodgings": lodgings,
+            "activities": activities,
+            "show_lodging_modal": True,
+        },
+    )
+
+
+@require_http_methods(["POST"])
+def add_activity(request, pk):
+    vacation = get_object_or_404(VacationPlan, pk=pk)
+    form = ActivityForm(request.POST)
+
+    if form.is_valid():
+        activity = form.save(commit=False)
+        activity.vacation = vacation
+        activity.save()
+        return redirect("vacation_detail", pk=pk)
+
+    flight_form = FlightForm()
+    lodging_form = LodgingForm()
+
+    flights = Flight.objects.filter(vacation=vacation)
+    lodgings = Lodging.objects.filter(vacation=vacation)
+    activities = Activity.objects.filter(vacation=vacation)
 
     return render(
         request,
@@ -46,45 +147,10 @@ def vacation_detail(request, pk):
             "vacation": vacation,
             "flight_form": flight_form,
             "lodging_form": lodging_form,
-            "activity_form": activity_form,
+            "activity_form": form,
             "flights": flights,
             "lodgings": lodgings,
             "activities": activities,
+            "show_activity_modal": True,
         },
     )
-
-
-@require_POST
-def add_flight(request, pk):
-    vacation = get_object_or_404(VacationPlan, pk=pk)
-    form = FlightForm(request.POST)
-    if form.is_valid():
-        flight = form.save(commit=False)
-        flight.vacation = vacation
-        flight.save()
-    return redirect("vacation_detail", pk=pk)
-
-
-@require_POST
-def add_lodging(request, pk):
-    vacation = get_object_or_404(VacationPlan, pk=pk)
-    form = LodgingForm(request.POST)
-    if form.is_valid():
-        lodging = form.save(commit=False)
-        lodging.vacation = vacation
-        lodging.save()
-    if not form.is_valid():
-        print("Lodging form errors:", form.errors)
-
-    return redirect("vacation_detail", pk=pk)
-
-
-@require_POST
-def add_activity(request, pk):
-    vacation = get_object_or_404(VacationPlan, pk=pk)
-    form = ActivityForm(request.POST)
-    if form.is_valid():
-        activity = form.save(commit=False)
-        activity.vacation = vacation
-        activity.save()
-    return redirect("vacation_detail", pk=pk)
